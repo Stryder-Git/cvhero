1fc4b175c98805185b5d590a54204eaa1d4e04bf
AzertQwert <113384273+AzertQwert@users.noreply.github.com>
2022-09-13 12:45:27 -0400

    Update pandas_printer.py

--- cvhero/pandas_printer.py
+++ cvhero/pandas_printer.py
@@ -124,6 +124,10 @@ def format_pandas(obj):
     else:
         raise ValueError("Only pd.DataFrame, -Series, and -Index are currently supported")
 
+def pandas_to_str(obj):
+    return str(obj)
+        
+        
 #
 # pd.DataFrame({
 # 	'bl': pd.Series([True, False, True]),
