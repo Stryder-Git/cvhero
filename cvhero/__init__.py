2c33784395a80a3523aeccbb05e3df5c0396dbee
Stryder-Git <marcel_pieper@hotmail.com>
2022-09-14 14:28:30 -0400

    added __version__

--- cvhero/__init__.py
+++ cvhero/__init__.py
@@ -1,3 +1,10 @@
+import pkg_resources
+# if running in development there may not be a package
+try:
+    __version__ = pkg_resources.get_distribution('cvhero').version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'development'
+
 from .pandas_printer import format_pandas
 from pyperclip import copy
 
